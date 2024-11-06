28a0ee81d97da0775c3e4e689537c5ee900e2f57
FluxedOverload <thanner.julius@outlook.de>
2024-11-06 21:13:04 +0100

    everforest theme

--- qtile/config.py
+++ qtile/config.py
@@ -24,7 +24,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# _                            _       
+
+# __       
 #(_)_ __ ___  _ __   ___  _ __| |_ ___ 
 #| | '_ ` _ \| '_ \ / _ \| '__| __/ __|
 #| | | | | | | |_) | (_) | |  | |_\__ \
@@ -34,26 +35,18 @@
 #-----------------------------------------
 # qtile-standard
 #-----------------------------------------
-import os
-import re
-import socket
-import subprocess
-import json
-from libqtile import hook
-from libqtile import qtile
-from typing import List  
-from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown, KeyChord
+from libqtile import bar, layout, qtile, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
-from libqtile.widget import Spacer, Backlight, LaunchBar
-from libqtile.widget.image import Image
-from libqtile.dgroups import simple_key_binder
-from pathlib import Path
-from libqtile.log_utils import logger
+#-----------------------------------------
+#Features
+#-----------------------------------------
 
+#-----------------------------------------
+# qtile-wayland
+#-----------------------------------------
 from libqtile.backend.wayland import InputConfig
-
 #-----------------------------------------
 # qtile-extras
 #-----------------------------------------
@@ -63,19 +56,26 @@ from qtile_extras.widget.decorations import PowerLineDecoration
 #from qtile_extras.layout.decorations import RoundedCorners
 
 
+
 # _  __          _     _           _ _                 
 #| |/ /___ _   _| |__ (_)_ __   __| (_)_ __   __ _ ___ 
 #| ' // _ \ | | | '_ \| | '_ \ / _` | | '_ \ / _` / __|
 #| . \  __/ |_| | |_) | | | | | (_| | | | | | (_| \__ \
 #|_|\_\___|\__, |_.__/|_|_| |_|\__,_|_|_| |_|\__, |___/
-#          |___/                             |___/     
+#          |___/                             |___/    
 
 mod = "mod4"
-mod1 = "Fn"
 terminal = guess_terminal()
+Browser = "firefox"
+HTB = "firefox hackthebox"
+Mail = "firefox outlook"
+FileManager = "thunar"
+Music = "spotify-launcher"
+
+
 
 keys = [
-#-----------------------------------------
+#----------------------------------------
 # Change Focus
 #-----------------------------------------
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
@@ -118,22 +118,19 @@ keys = [
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
 
 #-----------------------------------------
-# Tofi
+# Rofi
 #-----------------------------------------
-    Key([mod], "r", lazy.spawn("tofi-drun"), desc="Launch an application."),
-
+    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Launch an application."),
 #-----------------------------------------
 # Screenshot
 #-----------------------------------------
     Key([mod], "s", lazy.spawn("grim - | wl-copy",shell=True), desc="take screen scrennshot"),
     Key([mod, "shift"], "s", lazy.spawn('grim -g "$(slurp)" - | wl-copy', shell=True), desc="take area screenshot"),
-    
-#-----------------------------------------
-# Swaylock
-#-----------------------------------------
-    Key([mod], "o", lazy.spawn("swaylock --color '000000'"), desc="Trigger Lockscreen."), 
-
 ]
+
+
+
+
 
 # Add key bindings to switch VTs in Wayland.
 # We can't check qtile.core.name in default config as it is loaded before qtile is started
@@ -156,13 +153,58 @@ for vt in range(1, 8):
 #   \_/\_/ \___/|_|  |_|\_\___/ .__/ \__,_|\___\___||___/
 #                             |_|                        
 
-group_names = 'DESK(1) WWW(2) TERM(3) MAIL(4) OBSI(5) MP3(6) VM(7) RDP(8) VPN(9)'.split()
+group_names = '1 2 3 4 5 6 7 8 9'.split()
 groups = [Group(name, layout='max') for name in group_names]
 for i, name in enumerate(group_names):
     indx = str(i + 1)
     keys += [
         Key([mod], indx, lazy.group[name].toscreen()),
         Key([mod, 'shift'], indx, lazy.window.togroup(name))]
+
+####
+#Colors
+####
+
+
+#Normal
+color = [
+["#232a2e"], # 0  bg_dim
+["#2d353b"], # 1  bg0
+["#343f44"], # 2  bg1
+["#3d484d"], # 3  bg2
+["#475258"], # 4  bg3
+["#4f585e"], # 5  bg4
+["#56635f"], # 6  gb5 
+["#d3c6aa"], # 7  Foreground / Text / White
+["#e67e80"], # 8  Red
+["#e69875"], # 9  Orange
+["#dbbc7f"], # 10 Yellow
+["#a7c080"], # 11 Green
+["#83c092"], # 12 Cyan
+["#7fbbb3"], # 13 Blue
+["#d699b6"], # 14 Pink
+["#859289"], # 15 Grey
+]
+
+#Transparent
+color_tp = [
+["#232a2ef2"], # 0  bg_dim
+["#2d353bf2"], # 1  bg0
+["#343f44f2"], # 2  bg1
+["#3d484df2"], # 3  bg2
+["#475258f2"], # 4  bg3
+["#4f585ef2"], # 5  bg4
+["#56635ff2"], # 6  gb5 
+["#d3c6aaf2"], # 7  Foreground / Text / White
+["#e67e80f2"], # 8  Red
+["#e69875f2"], # 9  Orange
+["#dbbc7ff2"], # 10 Yellow
+["#a7c080f2"], # 11 Green
+["#83c092f2"], # 12 Cyan
+["#7fbbb3f2"], # 13 Blue
+["#d699b6f2"], # 14 Pink
+["#859289f2"], # 15 Grey
+]
 
 
 # _                            _       
@@ -173,11 +215,11 @@ for i, name in enumerate(group_names):
 #            |___/                     
 
 layout_theme = {
-    "border_focus": '#ff757f',
-    "border_normal":'#7dcfff',
-    "border_width":2,
-    "margin":10,
-    "margin_on_single":10,
+    "border_focus": color[11],
+    "border_normal": '#00000000',
+    "border_width":1,
+    "margin":8,
+    "margin_on_single":8,
 }
 
 layouts = [
@@ -188,83 +230,122 @@ layouts = [
         layout.Floating(),
 ]
 
-#__        ___     _            _       
-#\ \      / (_) __| | __ _  ___| |_ ___ 
+
+#__        ___     _            _
+#\ \      / (_) __| | __ _  ___| |_ ___
 # \ \ /\ / /| |/ _` |/ _` |/ _ \ __/ __|
 #  \ V  V / | | (_| | (_| |  __/ |_\__ \
 #   \_/\_/  |_|\__,_|\__, |\___|\__|___/
-#                    |___/              
+#                    |___/
 
 widget_defaults = dict(
-    font="JetBrainsMono Nerd Font Bold",
+    font="Jetbrains Mono Nerd Bold",
     fontsize=12,
-    padding=1,
+    padding=12,
 )
 extension_defaults = widget_defaults.copy()
-
 
 #----------------------------------------
 # Decorations
 #----------------------------------------
-decor_left = {
-    "decorations": [
-        PowerLineDecoration(
-            # path="arrow_left"
-            path="rounded_left"
-            # path="forward_slash"
-            # path="back_slash"
-        )
-    ],
-}
-
-decor_right = {
-    "decorations": [
-        PowerLineDecoration(
-            # path="arrow_right"
-            path="rounded_right"
-            # path="forward_slash"
-            # path="back_slash"
-        )
-    ],
-}
-
+#decor_left = {
+#    "decorations": [
+#        PowerLineDecoration(
+#            # path="arrow_left"
+#            path="rounded_left"
+#            # path="forward_slash"
+#            # path="back_slash"
+#        )
+#    ],
+#}
+#
+#decor_right = {
+#    "decorations": [
+#        PowerLineDecoration(
+#            # path="arrow_right"
+#            path="rounded_right"
+#            # path="forward_slash"
+#            # path="back_slash"
+#        )
+#    ],
+#}
+#
 decoration_group = {
     "decorations": [
         RectDecoration(
-            colour="#1a1b26F2",
-            radius=12,
+            colour= color_tp[1],
+            radius=10,
             filled=True,
-            padding_x=2,
-            group=False,
-            line_colour="#ff757fF2",
-            line_width=0,
+            padding_x=0,
+            group=True,
+            line_colour=color[9],
+            line_width=1.2,
             )
     ],
-    "padding": 10,
+    "padding": 5,
 }
+
 #-----------------------------------------
 # Widget List
 #-----------------------------------------
 widget_list = [
-    widget.Spacer(
-        length = 10,
-        background = '#00000000',
-    ),
-    widget.Image(
-        background = '#00000000',
-        filename = '~/.config/Icons/Arch_Logo_Red.png',
-        margin = 6,
-        mouse_callbacks = {"Button1": lazy.spawn(terminal)},
-        **decoration_group
-    ),
+    #widget.Image(
+    #    background = '#00000000',
+    #    filename = '/usr/share/Icons/Arch.png',
+    #    margin = 3,
+    #    mouse_callbacks = {"Button1": lazy.spawn(terminal)},
+#        **decoration_group
+    #),    
     widget.GroupBox(
         background = '#00000000',
-        active = '#7dcfff',
-        inactive = '#7aa2f7',
-        highlight_method='line',
-        highlight_color = '#00000000',
-        this_current_screen_border = '#ff757f',
-        block_highlight_text_color = '#ff757f',
+        active = color[7],
+        inactive = color[6],
+        highlight_method='text',
+        this_current_screen_border = color[11],
+        **decoration_group
+    ),
+    widget.Prompt(
+    ),
+#---------------------------------------
+widget.Spacer(
+        length = 10,
+        background = '#00000000'
+    ),
+widget.Image(
+        background = '#00000000',
+        filename = '/usr/share/Icons/Mail.png',
+        margin_y = 8,
+        margin_x = 3,
+        mouse_callbacks = {"Button1": lazy.spawn(Mail)},
+        **decoration_group
+    ),
+widget.Image(
+        background = '#00000000',
+        filename = '/usr/share/Icons/HTB.png',
+        margin_y = 8,
+        mouse_callbacks = {"Button1": lazy.spawn(HTB)},
+        **decoration_group
+    ),
+widget.Image(
+        background = '#00000000',
+        filename = '/usr/share/Icons/Music.png',
+        margin_y = 8,
+        mouse_callbacks = {"Button1": lazy.spawn(Music)},
+        **decoration_group
+    ),
+widget.Image(
+        background = '#00000000',
+        filename = '/usr/share/Icons/Firefox.png',
+        margin_y = 8,
+        mouse_callbacks = {"Button1": lazy.spawn(Browser)},
+        **decoration_group
+    ),
+widget.Image(
+        background = '#00000000',
+        filename = '/usr/share/Icons/Files.png',
+        margin_y = 8,
+        margin_x = 3,
+        mouse_callbacks = {"Button1": lazy.spawn(FileManager)},
         **decoration_group
     ),
 #---------------------------------------
@@ -272,97 +353,77 @@ widget_list = [
         length = bar.STRETCH,
         background = '#00000000'
     ),
-#---------------------------------------
-    widget.CheckUpdates(
+    widget.WindowName(
         background = '#00000000',
-        colour_have_updates = '#7dcfff',
-        colour_no_updates = '#7aa2f7',
-        distro = 'Arch',
-        execute = 'sudo pacman -Syu',
+        foreground = color[7],
         **decoration_group
     ),
-    widget.Wlan(
-        background = '#00000000', 
-        foreground = '#7dcfff',
-        format='WLan: {percent:2.0%}',
+    widget.Spacer(
+        length = bar.STRETCH,
+        background = '#00000000'
+    ),
+#---------------------------------------
+    widget.WiFiIcon(
+        background = '#00000000',
+        active_colour = color[7],
+        inacitve_colour = color[6],
+        disconnected_colour = color[8],
         interface = 'wlp0s20f3',
+        padding_y = 10,
+        padding_x = 3,
+        **decoration_group
+    ),
+    widget.Volume(
+        background = '#00000000',
+        foreground = color[7],
         **decoration_group
     ),
     widget.Battery(
         background = '#00000000',
-        foreground = '#7dcfff',
+        foreground = color[7],
         format='{char} | {percent:2.0%}',
         charge_char = '+',
         discharge_char = '-',
         empty_char = '±',
+        #charge_controller: lambda (0, 90)
         **decoration_group
     ),
-    widget.PulseVolume(
-        background = '#00000000',
-        foreground = '#7dcfff',
-        fmt="Vol: {}",
-        **decoration_group
-    ),
-    widget.CPU(
-        background = '#00000000',
-        foreground = '#7dcfff',
-        format="CPU: {load_percent}%",
-        **decoration_group
-    ),
-    widget.Memory(
-        background = '#00000000',
-        foreground = '#7dcfff',
-        measure_mem = 'G',
-        format="RAM: {MemUsed:.1f} | {MemTotal:.0f} GB",
-        **decoration_group
+    widget.Spacer(
+        length = 10,
+        background = '#00000000'
     ),
     widget.Clock(
         background = '#00000000',
-        foreground = '#7dcfff',
+        foreground = color[7],
+        margin = 3,
         format="%a %d.%h.%y | %H:%M",
         **decoration_group
     ),
-#    widget.Image(
-#        background = '#00000000',
-#        filename = '~/.config/Icons/Python_Logo_Red.png',
-#        margin = 6,
-#        mouse_callbacks = {"Button1": lazy.shutdown()},
-#        **decoration_group
-#    ),
-    widget.Spacer(
-        length = 10,
-        background = '#8CB7D900',
-    ),
+#    widget.StatusNotifier(),    
 ]
+
+
 # ____                               
 #/ ___|  ___ _ __ ___  ___ _ __  ___ 
 #\___ \ / __| '__/ _ \/ _ \ '_ \/ __|
 # ___) | (__| | |  __/  __/ | | \__ \
 #|____/ \___|_|  \___|\___|_| |_|___/
-                                    
 
 screens = [
     Screen(
         top=bar.Bar(
             widget_list,
             35,
-            border_width = [8, 0, 6, 0],
+            border_width = [10, 8, 2, 8],
             border_color = '#ffffff00',
         ),
         #Set Wallpaper
-        wallpaper = "/usr/share/Wallpapers/Tokyo_night_2.jpg",
-        #wallpaper = "~/.config/Wallpapers/Real_Mount.jpg",
+        wallpaper = "/usr/share/Wallpaper/Forest.jpg",
         wallpaper_mode = 'fill',
     ),
 ]
 
-
-# __  __                      
-#|  \/  | ___  _   _ ___  ___ 
-#| |\/| |/ _ \| | | / __|/ _ \
-#| |  | | (_) | |_| \__ \  __/
-#|_|  |_|\___/ \__,_|___/\___|
-                             
+# Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
@@ -376,8 +437,6 @@ bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
-        border_focus = '#ff757f',
-        border_normal = '#7dcfff',
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
@@ -389,7 +448,7 @@ floating_layout = layout.Floating(
         Match(title="pinentry"),  # GPG key password entry
     ]
 )
-auto_fullscreen = False
+auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
@@ -412,13 +471,3 @@ wl_input_rules = {
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
-
-# Hooks
-
-# HOOK startup
-@hook.subscribe.startup_once
-def autostart():
-    autostartscript = "~/.config/qtile/autostart.sh"
-    home = os.path.expanduser(autostartscript)
-    subprocess.Popen([home])
-
